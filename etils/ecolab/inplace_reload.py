573bab627e8e8d8f8e0281a29571e153d78ab758
Etienne Pot <epot@google.com>
2023-06-27 05:37:16 -0700

    Reimplement auto-reload
    PiperOrigin-RevId: 543715644

--- etils/ecolab/inplace_reload.py
+++ etils/ecolab/inplace_reload.py
@@ -18,6 +18,7 @@ from __future__ import annotations
 
 import collections
 import contextlib
+import dataclasses
 import enum
 import functools
 import gc
@@ -29,7 +30,6 @@ import weakref
 
 from etils import epy
 from etils.ecolab import module_utils
-from IPython.extensions import autoreload
 
 
 class ReloadMode(epy.StrEnum):
@@ -54,15 +54,62 @@ def get_reloader() -> _InPlaceReloader:
   return _InPlaceReloader()
 
 
+@dataclasses.dataclass(frozen=True)
+class _ModuleRefs:
+  """Reference on the previous module/object instances."""
+
+  modules: list[weakref.ref[types.ModuleType]] = dataclasses.field(
+      default_factory=list
+  )
+  objs: dict[str, list[weakref.ref[Any]]] = dataclasses.field(
+      default_factory=lambda: collections.defaultdict(list)
+  )
+
+  def save_module(self, module: types.ModuleType) -> None:
+    """Save reference on all previous modules/objects."""
+    self.modules.append(weakref.ref(module))
+
+    for name, old_obj in module.__dict__.items():
+      # Only update objects part of the module (filter other imported symbols)
+      if not _belong_to_module(old_obj, module):
+        continue
+
+      self.objs[name].append(weakref.ref(old_obj))
+
+  def update_refs_with_new_module(self, new_module: types.ModuleType) -> None:
+    """Update all old reference to previous objects."""
+    # Update all modules
+    for old_module in self.modules:
+      old_module = old_module()  # Resolve weakref
+      if old_module is None:  # No ref to update
+        continue
+      _update_old_module(old_module, new_module)
+
+    # Update all objects
+    for name, old_obj_refs in self.objs.items():
+      if name not in new_module.__dict__:
+        # TODO(epot): Could try to invalidate the object
+        continue
+
+      new_obj = new_module.__dict__[name]
+
+      for old_obj_ref in old_obj_refs:
+        old_obj = old_obj_ref()
+        if old_obj is None:
+          continue
+        # TODO(epot): Support cycles
+        _update_generic(old_obj, new_obj)
+
+
 class _InPlaceReloader:
   """Global manager which track all reloaded modules."""
 
   def __init__(self):
-    # Previously imported modules
-    # When a new module is `reloaded` with `invalidate='replace'`, all previous
+    # Previously imported modules / objects
+    # When a new module is `reloaded` with `UPDATE_INPLACE`, all previous
     # modules are replaced in-place.
-    self._previous_modules: dict[str, list[weakref.ref[types.ModuleType]]] = (
-        collections.defaultdict(list)
+    self._previous_modules: dict[str, _ModuleRefs] = collections.defaultdict(
+        _ModuleRefs
     )
 
   @contextlib.contextmanager
@@ -76,7 +123,7 @@ class _InPlaceReloader:
     """Eventually update old modules."""
     # Track imported modules before they are removed from cache (to update them
     # after reloading)
-    self._save_modules(reload=reload)
+    self._save_objs(reload=reload)
 
     # We clear the module cache to trigger a full reload import.
     # This is better than `colab_import.reload_package` as it support
@@ -97,43 +144,36 @@ class _InPlaceReloader:
         _update_old_modules(
             reload=reload,
             previous_modules=self._previous_modules,
-            verbose=verbose,
         )
 
-  def _save_modules(self, *, reload: list[str]) -> None:
-    """Save all modules."""
+  def _save_objs(self, *, reload: list[str]) -> None:
+    """Save all modules/objects."""
     # Save all modules
     for module_name in module_utils.get_module_names(reload):
       module = sys.modules.get(module_name)
       if module is None:
         continue
-      self._previous_modules[module_name].append(weakref.ref(module))
+
+      # Save module with all objects from the module
+      self._previous_modules[module_name].save_module(module)
 
 
 def _update_old_modules(
     *,
     reload: list[str],
-    previous_modules: dict[str, list[weakref.ref[types.ModuleType]]],
-    verbose: bool,
+    previous_modules: dict[str, _ModuleRefs],
 ) -> None:
   """Update all old modules."""
-  _mock_autoreload()
   for module_name in module_utils.get_module_names(reload):
     new_module = sys.modules[module_name]
-    for old_module in previous_modules.get(module_name, []):
-      old_module = old_module()  # Resolve weakref
-      if old_module is None:  # No ref to update
-        if verbose:
-          print(f"Skipping {module_name} (dead)")
-        continue
-      _update_old_module(old_module, new_module, verbose=verbose)
+    old_module_refs = previous_modules.get(module_name)
+    if old_module_refs is not None:
+      old_module_refs.update_refs_with_new_module(new_module)
 
 
 def _update_old_module(
     old_module: types.ModuleType,
     new_module: types.ModuleType,
-    *,
-    verbose: bool,
 ) -> None:
   """Mutate the old module version with the new dict.
 
@@ -143,55 +183,15 @@ def _update_old_module(
   Args:
     old_module: Old module to update
     new_module: New module
-    verbose: If `True`, display which objects are updated
   """
-  # Mutate individual classes, objects,... so previously created objects uses
-  # new code
-  for name, old_obj in old_module.__dict__.items():
-    if name not in new_module.__dict__:
-      # TODO(epot): Could still invalidate the function/class (to prevent
-      # missuse) ?
-      continue
-    # Only update objects part of the module (filter other imported symbols)
-    if not _bellong_to_module(old_obj, old_module):
-      continue
-
-    new_obj = new_module.__dict__[name]
-    if verbose:
-      if isinstance(old_obj, type):
-        old_obj_cls = old_obj
-      else:
-        old_obj_cls = type(old_obj)
-      print(f"Update {old_obj_cls.__name__} ({new_module.__name__})")
-    autoreload.update_generic(old_obj, new_obj)
-
-    # TODO(epot): Also update the `shell.user_ns`.
-
   # Replace the old dict by the new module content
   old_module.__dict__.clear()
   old_module.__dict__.update(new_module.__dict__)
 
 
-def _bellong_to_module(obj: Any, module: types.ModuleType) -> bool:
-  """Returns `True` if the instance, class, function bellong to module."""
+def _belong_to_module(obj: Any, module: types.ModuleType) -> bool:
+  """Returns `True` if the instance, class, function belong to module."""
   return hasattr(obj, "__module__") and obj.__module__ == module.__name__
-
-
-@functools.cache
-def _mock_autoreload() -> None:
-  """Add `update_instances` to autoreload."""
-
-  autoreload.update_class = _wrap_fn(autoreload.update_class, _new_update_class)
-  autoreload.update_generic = _wrap_fn(
-      autoreload.update_generic, _new_update_generic
-  )
-
-  # Also update tuple
-  assert autoreload.UPDATE_RULES[0][1].__name__ == "update_class"
-  autoreload.UPDATE_RULES[0] = (
-      autoreload.UPDATE_RULES[0][0],
-      autoreload.update_class,
-  )
 
 
 def _wrap_fn(old_fn, new_fn):
@@ -205,21 +205,78 @@ def _wrap_fn(old_fn, new_fn):
   return decorated
 
 
-def _new_update_class(fn, old, new):
-  # update_instances is only present in recent IPython. See:
-  # https://github.com/ipython/ipython/blob/main/IPython/extensions/autoreload.py
-  fn(old, new)
-  update_instances(old, new)
+def _update_class(old, new):
+  """Update the class."""
+
+  for key in list(old.__dict__.keys()):
+    old_obj = getattr(old, key)
+
+    try:
+      new_obj = getattr(new, key)
+    except AttributeError:
+      # obsolete attribute: remove it
+      try:
+        delattr(old, key)
+      except (AttributeError, TypeError):
+        pass
+      continue
+
+    _update_generic(old_obj, new_obj)
+
+    try:
+      setattr(old, key, getattr(new, key))
+    except (AttributeError, TypeError):
+      pass  # skip non-writable attributes
+
+  _update_instances(old, new)
 
 
-def _new_update_generic(fn, old, new):
+def _update_function(old, new):
+  """Upgrade the code object of a function."""
+  for name in [
+      "__code__",
+      "__defaults__",
+      "__doc__",
+      "__closure__",
+      "__globals__",
+      "__dict__",
+  ]:
+    try:
+      setattr(old, name, getattr(new, name))
+    except (AttributeError, TypeError):
+      pass
+
+
+def _update_property(old, new):
+  """Replace get/set/del functions of a property."""
+  _update_generic(old.fdel, new.fdel)
+  _update_generic(old.fget, new.fget)
+  _update_generic(old.fset, new.fset)
+
+
+def _update_generic(old, new):
   # Stop replacement if the 2 objects are the same
   if old is new:
-    return True
-  return fn(old, new)
+    return
+
+  for type_, update in _UPDATE_RULES:
+    if isinstance(old, type_) and isinstance(new, type_):
+      update(old, new)
+      return True
 
 
-def update_instances(old, new):
+_UPDATE_RULES = [
+    (type, _update_class),
+    (types.FunctionType, _update_function),
+    (
+        types.MethodType,
+        lambda a, b: _update_function(a.__func__, b.__func__),
+    ),
+    (property, _update_property),
+]
+
+
+def _update_instances(old, new):
   """Backport of `update_instances`."""
 
   refs = gc.get_referrers(old)
