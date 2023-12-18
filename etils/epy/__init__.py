1fc9678df177787bccb36bbacd715bdbe61c6e3f
Etienne Pot <epot@google.com>
2023-12-18 05:49:06 -0800

    Cleanup hack after the new XManager release
    PiperOrigin-RevId: 591870600

--- etils/epy/__init__.py
+++ etils/epy/__init__.py
@@ -18,8 +18,9 @@
 
 import sys
 
+from etils.epy import _internal
+from etils.epy.adhoc_utils.binary_import import binary_adhoc
 from etils.epy.backports import cached_property
-from etils.epy.binary_import import binary_adhoc
 from etils.epy.contextlib import ContextManager
 from etils.epy.env_utils import is_notebook
 from etils.epy.itertools import groupby
